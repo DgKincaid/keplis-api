719b72f9d56fea465966d584fac05329316635ad
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2020-04-10 19:03:22 -0400

    added global

--- src/db/sprint-db/sprint-db.module.ts
+++ src/db/sprint-db/sprint-db.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { SprintDbService } from './sprint-db.service';
 import { SprintSchema } from './Sprint';
 
+@Global()
 @Module({
   imports: [ MongooseModule.forFeature([{ name: 'Sprint', schema: SprintSchema }]) ],
   providers: [SprintDbService]
